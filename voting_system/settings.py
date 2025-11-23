8875e65b642654bdbb11e4b0039558a72aaae2df
antheaayaroda-source <antheaayaroda@gmail.com>
2025-11-23 22:13:28 +0800

    Fix INSTALLED_APPS order for Cloudinary

--- voting_system/settings.py
+++ voting_system/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'cloudinary_storage',
+    'django.contrib.staticfiles',
     'cloudinary',
     'voting',
 ]
