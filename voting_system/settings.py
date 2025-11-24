0b77d1acda2b1d2fe5edabcd9770b3cbb4ba8a59
antheaayaroda-source <antheaayaroda@gmail.com>
2025-11-24 10:06:01 +0800

    Add AWS S3 for media storage

--- voting_system/settings.py
+++ voting_system/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'voting',
 ]
 
@@ -134,14 +135,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Cloudinary configuration - disabled for now, using local media storage
-# if not DEBUG:
-#     cloudinary.config(
-#         cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
-#         api_key=os.environ.get('CLOUDINARY_API_KEY'),
-#         api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
-#         secure=True
-#     )
+# AWS S3 configuration for production media storage
+if not DEBUG:
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-1')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    AWS_DEFAULT_ACL = 'public-read'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Security settings for production
 if not DEBUG:
